96be3634e8741d6388a8e0e943e2f6afbb1e38d1
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-09-06 23:06:56 +0300

    Hello page will be rendered instead of sample app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run()
