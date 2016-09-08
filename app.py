fb61206fc27de8f234a9295216e9f048a3d10422
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-09-08 22:13:27 +0300

    Token exchange

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask from flask import render_template from flask import request
+import os
+import requests
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return render_template('hello.html')
+
+
+@app.route("/token_exchange")
+def token_exchange():
+    state = request.args.get('state', '')
+    code = request.args.get('code', '')
+
+    payload = {'client_id': os.environ['CLIENT_ID'],
+               'client_secret': os.environ['CLIENT_SECRET'],
+               'code': code}
+
+    r = requests.post('https://www.strava.com/oauth/token', data=payload)
+    return r.text
 
 if __name__ == "__main__":
     app.run()
