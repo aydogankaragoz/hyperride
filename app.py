a391291403a4132c9bfcdcd4fc56e67e975c278b
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-09-08 22:14:52 +0300

    Fixed imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask from flask import render_template from flask import request
+from flask import Flask, render_template, request
 import os
 import requests
 app = Flask(__name__)
