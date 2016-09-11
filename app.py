7647b3e8fc951f6b6e403164545d262166649342
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-09-11 18:30:01 +0300

    Enabled debug mode

--- app.py
+++ app.py
@@ -26,20 +26,18 @@ def token_exchange():
     username = response['athlete']['username']
     firstname = response['athlete']['firstname']
     lastname = response['athlete']['lastname']
-    
+
     profile_medium = response['athlete']['profile_medium']
     profile = response['athlete']['profile']
-    
+
     city = response['athlete']['city']
     state = response['athlete']['state']
     country = response['athlete']['country']
-    
+
     sex = response['athlete']['sex']
     email = response['athlete']['email']
 
-    created_at = response['athlete']['created_at']
-    updated_at = response['athlete']['updated_at']
     return profile + ' : ' + created_at 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
