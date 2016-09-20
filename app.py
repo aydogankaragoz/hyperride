785309c68308035c56ac95c76a8280ab802e01be
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-09-21 01:23:50 +0300

    Added blinker library as a requirement for Sentry

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import model
 import telegram
 from raven.contrib.flask import Sentry
 app = Flask(__name__)
-#sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
+sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
 
 
 def get_session():
