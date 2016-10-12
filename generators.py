3829d39fecf2a38abf3df07eedcc0fd2a43256b7
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-10-12 22:38:10 +0300

    Removing forgotten lines

--- generators.py
+++ generators.py
@@ -4,9 +4,6 @@ import telegram
 
 
 def analyse_activity(owner_id, activity_id):
-    resp = requests.get(url)
-    return len(resp.text.split())
-
     session = app.get_session()
     athlete = session.query(model.Athlete).get(owner_id)
 
