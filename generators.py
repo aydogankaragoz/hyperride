116d36c0713af26c6d3c8ce6f5ceb70d33549e73
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-10-13 22:23:19 +0300

    Generators has its own session

--- generators.py
+++ generators.py
@@ -1,8 +1,12 @@
 import model
 import telegram
+from sqlalchemy.orm import sessionmaker
 
+engine = model.connect_db()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-def analyse_activity(session, owner_id, activity_id):
+def analyse_activity(owner_id, activity_id):
     athlete = session.query(model.Athlete).get(owner_id)
 
     telegram.newActivity(athlete.firstname, athlete.lastname, activity_id)
